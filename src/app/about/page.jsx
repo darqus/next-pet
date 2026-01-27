e2c249956d9ac5fb1cd30b52cc984678c30e0c1a
darqus <voladores@inbox.ru>
2026-01-27 23:40:41 +0300

    feat(ui): add about page and global footer component
    - Create new about page with basic content structure
    - Implement reusable footer component with copyright notice
    - Integrate footer into main layout to display site-wide

---
+++ src/app/about/page.jsx
@@ -0,0 +1,15 @@
+const AboutPage = () => {
+  return (
+    <section className="content">
+      <h2 className="section-title">About</h2>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci at
+        suscipit rem fugit ipsum beatae recusandae repudiandae excepturi libero,
+        dolor tempora provident eius itaque enim maiores eum et blanditiis
+        porro.
+      </p>
+    </section>
+  )
+}
+
+export default AboutPage
---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <footer className='container footer'>
+      <span>© L-o-D</span>
+    </footer>
+  )
+}
+
+export default Footer
