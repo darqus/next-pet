9803a80dd65f06af88f5ef45e02d269e50011fd1
darqus <voladores@inbox.ru>
2026-01-31 16:47:31 +0300

    refactor(imports): update relative import to use path alias
    - Replace relative import path `../../lib/hello` with `@/lib/hello` alias
    - Update section title from "Get request" to "Post request" for accuracy

--- src/app/postExample/page.jsx
+++ src/app/postExample/page.jsx
@@ -1,11 +1,11 @@
 import ButtonGetRequest from '@/components/ButtonGetRequest'
-import { createHelloGreeting } from '../../lib/hello'
+import { createHelloGreeting } from '@/lib/hello'
 
 const PostExample = () => {
   const { message } = createHelloGreeting('John Dow')
   return (
     <section className="content">
-      <h2 className="section-title">Get request</h2>
+      <h2 className="section-title">Post request</h2>
       {message && <p>{message}</p>}
       <ButtonGetRequest />
     </section>
